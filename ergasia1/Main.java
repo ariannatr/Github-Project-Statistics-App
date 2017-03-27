3c39c9ba10385c704bcaa9b3c5ff2237c220d0ba
stavrinasof <staurinasof@gmail.com>
2017-03-28 02:30:24 +0300

    html almost done

--- ergasia1/Main.java
+++ ergasia1/Main.java
@@ -6,28 +6,31 @@ public class Main {
 
    public static void main(String []args) throws IOException{
      
-      	if(args.length <2)
-      	{	
+        if(args.length <2)
+        {	
       		System.err.println("Not enough arguments");
       		System.exit(1);
       	}
       	String filename=new String(args[0]);
       	File directory=new File(filename);
       	Operations op=new Operations(filename);
-      	System.out.println("Files:"+op.get_files_num(directory));
-      	
-	System.out.println("Lines: "+op.get_lines(directory,filename));
-      	System.out.println("Branches:"+op.get_branches(directory));
-      	System.out.println("Tags:"+op.get_tags(directory));
-      	System.out.println("Commiters:"+op.get_commiters(directory));
-	System.out.println("Branches analytical :");	
-	op.get_full_branches(directory);
-	System.out.println("Commits statistics  :");
-	op.get_commits(directory);
-	op.get_percentage_changes(directory);
+        Myhtml html=new Myhtml(args[1]);
 
-   System.out.println("Commits statistics per day month year");
-   op.get_commit_statistics_per_author(directory);
+      	html.print_files(op.get_files_num(directory));
+        html.print_lines(op.get_lines(directory,filename));
+	    html.print_branches_tags_commiters(op.get_branches(directory),op.get_tags(directory),op.get_commiters(directory));
+        html.start_full_branches();
+        System.out.println("Branches analytical :");	
+	   op.get_full_branches(directory,html);
+       html.end_table();
+	   System.out.println("Commits statistics  :");
+	   op.get_commits(directory,html);
+	   op.get_percentage_changes(directory,html);
 
+       System.out.println("Commits statistics per day month year");
+        op.get_commit_statistics_per_author(directory,html);
+
+       html.close_html();
+   
    }
 }
