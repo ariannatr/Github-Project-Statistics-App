11808f85146fcdd9fe6d2f30c5a7c07eec3a2eb9
ariannatr <sdi1300179@di.uoa.gr>
2017-03-27 23:37:37 +0300

    Add files via upload

--- ergasia1/Main.java
+++ ergasia1/Main.java
@@ -11,13 +11,12 @@ public class Main {
       		System.err.println("Not enough arguments");
       		System.exit(1);
       	}
-	int lines=0;
       	String filename=new String(args[0]);
       	File directory=new File(filename);
       	Operations op=new Operations(filename);
       	System.out.println("Files:"+op.get_files_num(directory));
-      	op.get_lines(directory,filename,lines);
-	System.out.println("Lines: "+lines);
+      	
+	System.out.println("Lines: "+op.get_lines(directory,filename));
       	System.out.println("Branches:"+op.get_branches(directory));
       	System.out.println("Tags:"+op.get_tags(directory));
       	System.out.println("Commiters:"+op.get_commiters(directory));
@@ -26,5 +25,9 @@ public class Main {
 	System.out.println("Commits statistics  :");
 	op.get_commits(directory);
 	op.get_percentage_changes(directory);
+
+   System.out.println("Commits statistics per day month year");
+   op.get_commit_statistics_per_author(directory);
+
    }
 }
